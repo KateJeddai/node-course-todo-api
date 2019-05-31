08302e158a7e646bba135170ae0eacb38e28fa81
unknown <katiajaafar28@gmail.com>
2019-05-31 16:11:49 +0300

    added patch todo

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const {mongoose} = require('./db/mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -40,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
 	    	   return res.status(404).send();
 	    	}
             res.send({todo});
-	    }).catch(e => res.status(404).send());
+	    }).catch(e => res.status(400).send());
 	}
 	else return res.status(404).send();
 })
@@ -53,10 +55,37 @@ app.delete('/todos/:id', (req, res) => {
 				return res.status(404).send();
 			}
 			res.send({todo});
-		}).catch(e => res.status(404).send());
+		}).catch(e => res.status(400).send());
 	}
 	else return res.status(404).send();
+}) 
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if(!todo) {
+        	return res.status(404).send();
+        }
+
+        res.send({todo});
+        
+    }).catch(e => res.status(400).send());
 })
+
+
 
 app.listen(port, () => {
 	console.log('Started on port ' + port);
